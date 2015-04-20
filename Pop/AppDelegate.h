0c22d88e526053a9c77bb5b1667d933a1273b6fd
Charlin_冯成林 <1761904945@qq.com>
2015-04-20 15:48:11 +0800

    Initial Commit

---
+++ Pop/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Pop
+//
+//  Created by 冯成林 on 15/4/20.
+//  Copyright (c) 2015年 冯成林. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
